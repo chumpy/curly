19195c2da28144c31e62874a9428c4528e41b2ba
Kevin Beddingfield <kevin.beddingfield@gmail.com>
2012-07-28 11:32:08 -0700

    awesome icon from Vadim

--- Curly/main.m
+++ Curly/main.m
@@ -3,7 +3,7 @@
 //  Curly
 //
 //  Created by Kevin Beddingfield on 4/12/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Kevin Beddingfield. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
